bb46af54370bbc8c439ecbd40554b6d6dead72a1
TitoNadar <titonadar06@gmail.com>
2018-09-01 22:17:20 +0530

    election contract mapping done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,2 +1,29 @@
 
 pragma solidity ^0.4.24;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+}
