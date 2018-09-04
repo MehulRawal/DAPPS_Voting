7fd42e4f89f9ea07423500d3f54b0d0c0e37f71b
TitoNadar <titonadar06@gmail.com>
2018-09-04 13:35:40 +0530

    tests performed

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -17,6 +17,12 @@ contract Election {
     // Store Candidates Count
     uint public candidatesCount;
 
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+
     constructor () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
@@ -25,5 +31,22 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        votedEvent(_candidateId);
     }
 }
